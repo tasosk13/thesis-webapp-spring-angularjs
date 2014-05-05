bbb1152a600d5fefe61fb59b47cb9afd3e7ceb7e
Petros Makris <petmakris@gmail.com>
2014-05-05 22:53:46 +0300

    Moving back-end

--- front-end/app/scripts/controllers/main.js
+++ front-end/app/scripts/controllers/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('frontEndApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+
+        $scope.welcomeMessages = [{
+            header: "None",
+            message: "None"
+            }];
+
+        $http.get('http://localhost:8080/api/home/welcome')
+            .success(function (data, status, headers, config) {
+                $scope.welcomeMessages = data;
+            })
+            .error(function (data, status, headers, config) {
+                console.log('Error of rest call');
+            });
+    });
