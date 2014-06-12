709f84ccd8545b9c5d2a42a1e3a7f4f9839243f1
Nikolaos Kalodimas <nkalodimas@gmail.com>
2014-06-13 01:26:48 +0300

    Fixed settings and dependencies
    * Added dependency for the Entity Manager
    * Added dependencies for jdbc and mysql
    * Added persistence.xml
    * Fixed app-context's settings

--- src/main/java/gr/uoa/di/scan/thesis/repository/UserRepository.java
+++ src/main/java/gr/uoa/di/scan/thesis/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package gr.uoa.di.scan.thesis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import gr.uoa.di.scan.thesis.entity.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
