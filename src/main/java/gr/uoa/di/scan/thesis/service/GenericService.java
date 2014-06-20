bc145fdfbc051115e1ffb189799a33c18f325232
Petros Makris <petmakris@gmail.com>
2014-06-20 13:30:54 +0300

    Moved tests in src/ (moved from src/main/)

--- src/main/java/gr/uoa/di/scan/thesis/service/GenericService.java
+++ src/main/java/gr/uoa/di/scan/thesis/service/GenericService.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface GenericService< T, ID extends Serializable > {
+    
 	public T create(T t);
 	public T findByID(ID id);
 	public List<T> findAll();
